6ac3af4942a602ce6d464e9d4cb6fb76bd24b337
PaulFournier <44707149+Paul1465@users.noreply.github.com>
2021-04-20 19:00:01 +0200

    management empty data form on submit

--- src/Form/UserFormType.php
+++ src/Form/UserFormType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Doctrine\DBAL\Types\TextType;
+use App\Form\ApplicationType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,12 +15,30 @@ class UserFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('first_name')
-            ->add('last_name')
-            ->add('picture')
-            ->add('introduction')
-            ->add('description')
-            ->add('slug')
+            ->add('first_name',null, [
+                'empty_data' => 'Nom inconnu',
+                'label' => 'Nom'
+            ])
+            ->add('last_name',null, [
+                'empty_data' => 'Prénom inconnu',
+                'label' => 'Prénom'
+            ])
+            ->add('picture',null, [
+                'empty_data' => 'Pas de photo',
+                'label' => 'Photo'
+            ])
+            ->add('introduction',null, [
+                'empty_data' => 'Pas d\'introduction',
+                'label' => 'Introduction'
+            ])
+            ->add('description',null, [
+                'empty_data' => 'Pas de description',
+                'label' => 'Description'
+            ])
+            ->add('slug',null, [
+                'empty_data' => 'Pas de slug',
+                'label' => 'Slug'
+            ])
             ->add('save', SubmitType::class, [
                 'attr' => [
                     'class' => 'btn btn-primary mt-3'
